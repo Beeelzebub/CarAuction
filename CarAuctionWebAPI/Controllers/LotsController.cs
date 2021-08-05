23d7a7263d7dac2747869a4c285b16ae46ac2854
mkrtchke <mkrtchke@yandex.com>
2021-08-05 16:27:22 +0300

    add lotscontroller

---
+++ CarAuctionWebAPI/Controllers/LotsController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entity;
+using Entity.DTO;
+using Entity.Models;
+
+namespace CarAuctionWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LotsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly CarAuctionContext _carAuctionContext;
+
+        public LotsController(IMapper mapper, CarAuctionContext carAuctionContext) 
+        {
+            _mapper = mapper;
+            _carAuctionContext = carAuctionContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllLots()
+        {
+            var lots = _carAuctionContext.Lots.ToList();
+
+
+            return Ok(lots);
+        }
+
+    }
+}
