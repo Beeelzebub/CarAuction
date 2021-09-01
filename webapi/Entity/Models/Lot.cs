ed7016d784913f896b079490d2c0007aea082df9
Александр Понтус <sanchesik@bk.ru>
2021-09-01 14:24:41 +0300

    1

--- webapi/Entity/Models/Lot.cs
+++ webapi/Entity/Models/Lot.cs
@@ -3,30 +3,20 @@ using System.Collections.Generic;
 
 namespace Entity.Models
 {
-    public class Lot
+    public class Lot : IEntity
     {
         public int Id { get; set; }
-        
-        public DateTime StartDate { get; set; } 
-
-        public DateTime EndDate { get; set; } 
-        
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public decimal MinimalStep { get; set; }
-        
         public decimal StartingPrice { get; set; }
-        
         public decimal CurrentCost { get; set; }
         public decimal RedemptionPrice { get; set; }
-        
         public virtual Car Car { get; set; }
-
         public User Seller { get; set; } 
         public string SellerId { get; set; } 
         public LotStatus Status { get; set; }
-
-
         public ICollection<Bid> Bids { get; set; } = new List<Bid>();
-
     }
 
     public enum LotStatus
