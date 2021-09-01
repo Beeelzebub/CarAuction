966ae4d6db96519605d7a4170599578e7853b023
mkrtchke <mkrtchke@yandex.com>
2021-09-01 10:20:44 +0300

    Add validation filter attribute

---
+++ WebAPI/CarAuctionWebAPI/ActionFilters/ValidationFilterAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Contracts;
+using Entity;
+using Entity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+
+namespace CarAuctionWebAPI.ActionFilters
+{
+    public class ValidationFilterAttribute : IActionFilter
+    {
+        private readonly IRepositoryManager _repositoryManager;
+
+        public ValidationFilterAttribute(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var id = (int)context.ActionArguments["id"];
+            
+            if(id == 0)
+            {
+                context.Result = new BadRequestObjectResult("Bad id parameter");
+                return;
+            }
+            var car = _repositoryManager.Car.GetCarAsync(id); 
+            if (car == null)
+            {
+                context.Result = new BadRequestObjectResult("Object is null");
+            }
+            else
+            {
+                context.HttpContext.Items.Add("car", car);
+            }
+        }
+    }
+}
