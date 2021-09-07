0017d321a3e65b1551389673db34bfd466174635
Александр Понтус <sanchesik@bk.ru>
2021-09-07 16:46:47 +0300

    qw

--- WebAPI/Services/Exceptions/NotFoundException.cs
+++ WebAPI/Services/Exceptions/NotFoundException.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Services.Exceptions
 {
-    public class NotFoundException : Exception
+    public class NotFoundException : CustomException
     {
         public NotFoundException(string message)
             :base(message)
